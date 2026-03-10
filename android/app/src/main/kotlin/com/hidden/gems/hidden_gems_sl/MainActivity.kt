68c4cf0c89e8d50224366854105fb24c19be20db
sehas <pathiranasehas988@gmail.com>
2026-03-11 03:24:45 +0530

    feat: Create MainActivity extending FlutterFragmentActivity.

--- android/app/src/main/kotlin/com/hidden/gems/hidden_gems_sl/MainActivity.kt
+++ android/app/src/main/kotlin/com/hidden/gems/hidden_gems_sl/MainActivity.kt
@@ -7,6 +7,5 @@ import io.flutter.embedding.android.FlutterFragmentActivity
 class MainActivity : FlutterFragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        window.addFlags(WindowManager.LayoutParams.FLAG_SECURE)
     }
 }
